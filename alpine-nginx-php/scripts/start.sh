752d64734eee6090e8c3a7ac81fd4a71d5b8d5a0
mikey.zhaopeng <imzhpe@qq.com>
2018-10-24 14:33:22 +0800

    1

--- alpine-nginx-php/scripts/start.sh
+++ alpine-nginx-php/scripts/start.sh
@@ -2,72 +2,11 @@
 
 # Disable Strict Host checking for non interactive git clones
 
-mkdir -p -m 0700 /root/.ssh
-# Prevent config files from being filled to infinity by force of stop and restart the container 
-echo "" > /root/.ssh/config
-echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
-
-if [[ "$GIT_USE_SSH" == "1" ]] ; then
-  echo -e "Host *\n\tUser ${GIT_USERNAME}\n\n" >> /root/.ssh/config
-fi
-
-if [ ! -z "$SSH_KEY" ]; then
- echo $SSH_KEY > /root/.ssh/id_rsa.base64
- base64 -d /root/.ssh/id_rsa.base64 > /root/.ssh/id_rsa
- chmod 600 /root/.ssh/id_rsa
-fi
-
 # Set custom webroot
 if [ ! -z "$WEBROOT" ]; then
  sed -i "s#root /var/www/html;#root ${WEBROOT};#g" /etc/nginx/sites-available/default.conf
 else
  webroot=/var/www/html
-fi
-
-# Setup git variables
-if [ ! -z "$GIT_EMAIL" ]; then
- git config --global user.email "$GIT_EMAIL"
-fi
-if [ ! -z "$GIT_NAME" ]; then
- git config --global user.name "$GIT_NAME"
- git config --global push.default simple
-fi
-
-# Dont pull code down if the .git folder exists
-if [ ! -d "/var/www/html/.git" ]; then
- # Pull down code from git for our site!
- if [ ! -z "$GIT_REPO" ]; then
-   # Remove the test index file if you are pulling in a git repo
-   if [ ! -z ${REMOVE_FILES} ] && [ ${REMOVE_FILES} == 0 ]; then
-     echo "skiping removal of files"
-   else
-     rm -Rf /var/www/html/*
-   fi
-   GIT_COMMAND='git clone '
-   if [ ! -z "$GIT_BRANCH" ]; then
-     GIT_COMMAND=${GIT_COMMAND}" -b ${GIT_BRANCH}"
-   fi
-
-   if [ -z "$GIT_USERNAME" ] && [ -z "$GIT_PERSONAL_TOKEN" ]; then
-     GIT_COMMAND=${GIT_COMMAND}" ${GIT_REPO}"
-   else
-    if [[ "$GIT_USE_SSH" == "1" ]]; then
-      GIT_COMMAND=${GIT_COMMAND}" ${GIT_REPO}"
-    else
-      GIT_COMMAND=${GIT_COMMAND}" https://${GIT_USERNAME}:${GIT_PERSONAL_TOKEN}@${GIT_REPO}"
-    fi
-   fi
-   ${GIT_COMMAND} /var/www/html || exit 1
-   if [ ! -z "$GIT_TAG" ]; then
-     git checkout ${GIT_TAG} || exit 1
-   fi
-   if [ ! -z "$GIT_COMMIT" ]; then
-     git checkout ${GIT_COMMIT} || exit 1
-   fi
-   if [ -z "$SKIP_CHOWN" ]; then
-     chown -Rf nginx.nginx /var/www/html
-   fi
- fi
 fi
 
 # Enable custom nginx config files if they exist
@@ -78,11 +17,6 @@ fi
 if [ -f /var/www/html/conf/nginx/nginx-site.conf ]; then
   cp /var/www/html/conf/nginx/nginx-site.conf /etc/nginx/sites-available/default.conf
 fi
-
-if [ -f /var/www/html/conf/nginx/nginx-site-ssl.conf ]; then
-  cp /var/www/html/conf/nginx/nginx-site-ssl.conf /etc/nginx/sites-available/default-ssl.conf
-fi
-
 
 # Prevent config files from being filled to infinity by force of stop and restart the container
 lastlinephpconf="$(grep "." /usr/local/etc/php-fpm.conf | tail -1)"
@@ -110,16 +44,6 @@ if [[ "$REAL_IP_HEADER" == "1" ]] ; then
  sed -i "s/#set_real_ip_from/set_real_ip_from/" /etc/nginx/sites-available/default.conf
  if [ ! -z "$REAL_IP_FROM" ]; then
   sed -i "s#172.16.0.0/12#$REAL_IP_FROM#" /etc/nginx/sites-available/default.conf
- fi
-fi
-# Do the same for SSL sites
-if [ -f /etc/nginx/sites-available/default-ssl.conf ]; then
- if [[ "$REAL_IP_HEADER" == "1" ]] ; then
-  sed -i "s/#real_ip_header X-Forwarded-For;/real_ip_header X-Forwarded-For;/" /etc/nginx/sites-available/default-ssl.conf
-  sed -i "s/#set_real_ip_from/set_real_ip_from/" /etc/nginx/sites-available/default-ssl.conf
-  if [ ! -z "$REAL_IP_FROM" ]; then
-   sed -i "s#172.16.0.0/12#$REAL_IP_FROM#" /etc/nginx/sites-available/default-ssl.conf
-  fi
  fi
 fi
 
